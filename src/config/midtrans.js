c1514c734806b544e9c95af846ce6019afd7f8ba
fhazwan <siswayangtidakmencolok@gmail.com>
2026-04-10 22:47:45 +0700

    Pembuatan File .env: Seluruh kunci rahasia (Merchant ID, Client Key, Server Key) kini disimpan di satu tempat

--- src/config/midtrans.js
+++ src/config/midtrans.js
@@ -1,9 +1,9 @@
 // src/config/midtrans.js
 
 export const MIDTRANS_CONFIG = {
-  MERCHANT_ID: 'M583104013',
-  CLIENT_KEY:  'Mid-client-Gv9FDQ7k4_1lykVi',
-  SERVER_KEY:  'Mid-server-mg5SbqeMBtiW4p4_qgaJl1Rn',
+  MERCHANT_ID: process.env.EXPO_PUBLIC_MIDTRANS_MERCHANT_ID,
+  CLIENT_KEY:  process.env.EXPO_PUBLIC_MIDTRANS_CLIENT_KEY,
+  SERVER_KEY:  process.env.EXPO_PUBLIC_MIDTRANS_SERVER_KEY,
   
   // URL untuk Sandbox (Ganti jika sudah Live)
   BASE_URL: 'https://app.sandbox.midtrans.com/snap/v1/transactions',
